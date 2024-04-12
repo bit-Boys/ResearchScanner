811d9f2d94c0c797a5e9c0b176336b4a650e662f
bit-Boys <benjaminwellborn@gmail.com>
2024-04-12 08:42:04 -0400

    changes

--- main.py
+++ main.py
@@ -13,20 +13,34 @@ report = "*********************************************" \
          "general purpose cyber security assessment tool" \
          "Keep in mind false positives may occur, so test" \
          "Cases yourself. Tests SQL, XSS, Wordpress and " \
-         "more. \n" # global final response given
+         "more. \n"  # global final response given
 
 
 def main():
-
     # collect all the args
 
-    DirectoryEnum = False
-    wordlist = ""
-    url = "http://127.0.0.1:80"
+    parser = argparse.ArgumentParser(
+        prog='General Vulnerability Scanner',
+        description='Scans for common vulnerabilities, incuding SQL, XSS and Wordpress version',
+        epilog='')
 
+    parser.add_argument("url")
+    parser.add_argument('-d', '--direct',
+                        action='store_true')
+    parser.add_argument('-q', '--sql', action='store_true')
+    parser.add_argument('-x', '--xss', action='store_true')
+    parser.add_argument('-a', '--all', action='store_true')
+    parser.add_argument('-w', '--wordlist')
 
+    args = parser.parse_args()
 
+    DirectoryEnum = ""
+    if args.direct or args.all:
+        DirectoryEnum = False
 
+        wordlist = args.wordlist
+
+    url = args.url
 
     if DirectoryEnum:
         if os.path.isfile(wordlist):
@@ -35,22 +49,21 @@ def main():
         else:
             print("Please select a wordlist, or deselect directory enumeration")
 
+    if args.sql or args.all:
+        test_sql()
 
-    # call functions
-    test_sql()
 
-
+    if args.xss or args.all:
+        test_xss()
 
 
 def test_sql(url):
-
     # standard list of SQL injections.txt
     global report
     injections = []
     with open('injections.txt') as my_file:
         for line in my_file:
             injections.append(line)
-
 
     # 1. Take site and find all form elements. Get urls of these to post to and put in array
 
@@ -64,22 +77,18 @@ def test_sql(url):
 
     forms = soup.find_all('form')
 
-
-
-
     # 2. See if injections.txt provides positive result. Two techniques that can be used
     technique = 0
     for form in forms:
         # First, take two known incorrect injections.txt
-        wrongOne = requests.post(url, {form.name:"wrongpass"})
+        wrongOne = requests.post(url, {form.name: "wrongpass"})
         time.sleep(500)
-        wrongTwo = requests.post(url, {form.name:"anotherwrongpass"})
+        wrongTwo = requests.post(url, {form.name: "anotherwrongpass"})
 
-
-        if wrongOne == wrongTwo: # if these equal, it stands to reason all incorrect responses are
+        if wrongOne == wrongTwo:  # if these equal, it stands to reason all incorrect responses are
             technique = 1
         else:
-            technique = 2 # assume that a technique worked if it takes more than the length between the tests longer
+            technique = 2  # assume that a technique worked if it takes more than the length between the tests longer
             # may result in false positives or negatives
 
             greater = max(wrongOne.elapsed.total_seconds(), wrongTwo.elapsed.total_seconds())
@@ -88,32 +97,28 @@ def test_sql(url):
         if technique == 1:
 
             for injection in injections:
-                attempt = requests.post(url, {form.name:injection})
+                attempt = requests.post(url, {form.name: injection})
                 if attempt.text != wrongOne.text:
                     report = report + "Sql Injection using:  " + injection
 
-
         if technique == 2:
             for injection in injections:
-                attempt = requests.post(url, {form.name:injection})
+                attempt = requests.post(url, {form.name: injection})
 
-                if attempt.elapsed.total_seconds() > greater + (2* between):
+                if attempt.elapsed.total_seconds() > greater + (2 * between):
                     report = report + "Sql Injection Suspected using:  " + injection
 
 
-
 def contactUser(content):
-
     webhook = DiscordWebhook(url="urltoserverwebhook", content=content)
     response = webhook.execute()
 
-def gui():
 
+def gui():
     # creates simple gui
     win = tk.Tk()
 
-    Label(win, text="site URL" ).grid(row=0)
-
+    Label(win, text="site URL").grid(row=0)
 
 
 def wordpressEnum(url):
@@ -121,24 +126,23 @@ def wordpressEnum(url):
     r = requests.get(url)
     soup = BeautifulSoup.soup(r, "html.parser")
 
-    version = soup.find_all('meta', name="generator")     # finds the generator tag
+    version = soup.find_all('meta', name="generator")  # finds the generator tag
     if version[0].content.contains("Wordpress"):
-
         wpSys = requests.get(f'https://www.wpvulnerability.net/core/{version[0].content}/')
 
         report = report + "Generator tag open. Found vulnerabilities as follows: " + wpSys
+
 
 def directoryBust(url, wordlist):
     # havnt yet impemented incursive search
     # also threading for speed
 
-
     i = 0
-    for char in url:          # probably some tricky way to do with string manipulation
+    for char in url:  # probably some tricky way to do with string manipulation
         if char == "/":
             break
         else:
-            i = i + 1        # code editor hates i++
+            i = i + 1  # code editor hates i++
 
     baseURL = url[:i]
 
@@ -150,16 +154,13 @@ def directoryBust(url, wordlist):
             if r.status_code == 200:
                 correct.append(f'{baseURL}/{line}')
 
-            time.sleep(1400) # What is minimum acceptable, may need just a short wordlist
+            time.sleep(1400)  # What is minimum acceptable, may need just a short wordlist
     return correct
 
-
+def test_xss():
+    pass
 
 # Bust domains
 # nmap thing to get flags?
 # docs
 # miters cvs
-
-
-
-
